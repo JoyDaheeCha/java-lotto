785b105af7e403addd1711d1a2c57fdf4b1c8ee1
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-20 09:42:25 +0900

    test: 'wholeNumber 정상적으로 생성' 테스트 코드 추가

--- src/test/java/StringCalculator/WholeNumberTest.java
+++ src/test/java/StringCalculator/WholeNumberTest.java
@@ -2,8 +2,11 @@ package StringCalculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WholeNumberTest {
 
@@ -13,5 +16,12 @@ public class WholeNumberTest {
         assertThatThrownBy(() -> new WholeNumber(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageMatching("음수는 덧셈 대상이 될 수 없습니다.");
+    }
+
+    @DisplayName("WholeNumber 정상적으로 생성")
+    @ParameterizedTest(name="param: 정수 {0}")
+    @ValueSource(ints = {1,2,3})
+    void successfully(int param) {
+        assertThat(new WholeNumber(param)).isEqualTo(new WholeNumber(param));
     }
 }
